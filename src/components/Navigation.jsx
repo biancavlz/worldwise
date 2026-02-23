ffd570bb71aabc699d9ef3353d3e5098581ecf4f
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-23 13:42:18 +0100

    Add css navigation module

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Navigation.module.css";
 
 function Navigation() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
