27866e91638b4ff4e474c933c8ec1d39680675d0
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-25 14:07:07 +0100

    Create custom CitiesContext and reuse it in  Country and cities components

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import styles from "./countryList.module.css";
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { isLoading, cities } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
@@ -18,7 +21,7 @@ function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
