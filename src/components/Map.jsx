810f78300821f6b62b3a7a21fb8ba01438b6326e
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-26 11:18:37 +0100

    Add city map marker and pop with city name

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,11 +2,13 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./map.module.css";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   // const [searchParams, setSearchParams] = useSearchParams();
   // const navigate = useNavigate();
   const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
 
   // const lat = searchParams.get("lat");
   // const lng = searchParams.get("lng");
@@ -23,11 +25,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
